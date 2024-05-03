1527a62b9501edb07cd1fd10bdbb661b20f7cadf
hunglydev <lyhaihung2003@gmail.com>
2024-05-03 16:02:04 +0700

    first commit

---
+++ lib/constant/constant.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color primaryColor = Color.fromRGBO(8, 91, 87, 1);
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:money_management/constant/constant.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State createState() {
+    return HomeScreenState();
+  }
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
